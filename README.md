a92624c42c537ba12ee6f3488fac7b2a4bcf9359
perchik_air <1919ars@gmail.com>
2021-12-15 07:22:38 +0200

    (air)

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Learning asyncio in Flask
 
+Best for fetching several http requests in parallel.
+
 ## Credits
 
 - [stackoverflow | pip install flask[async]](https://stackoverflow.com/questions/30539798/zsh-no-matches-found-requestssecurity)
+- [blog | Async in Flask 2.0](https://testdriven.io/blog/flask-async/)
+- [blog | aiohttp](https://www.twilio.com/blog/asynchronous-http-requests-in-python-with-aiohttp)
--- app.py
+++ app.py
@@ -1,23 +1,47 @@
-from flask import Flask
+import time
+
+from flask import Flask, render_template, request
 import asyncio
 
 app = Flask(__name__)
 
+data_list_sync = []
+data_list_async = []
 
-async def async_get_data():
-    await asyncio.sleep(1)
-    return 'Done!'
+
+async def async_get_data(name):
+    await asyncio.sleep(5)
+    data_list_async.append(name)
+    return f'Input value: {data_list_async} '
 
 
 @app.route("/data")
 async def get_data():
-    data = await async_get_data()
-    return data
+    tasks = []
+    for i in range(10):
+        task = asyncio.create_task(async_get_data(i))
+        tasks.append(task)
+    data = await asyncio.gather(*tasks)
+    return render_template('async_check.html', data=data)
+
+
+def get_data_func_internal(name):
+    time.sleep(5)
+    data_list_sync.append(name)
+    return f'Input value: {data_list_sync} '
+
+
+@app.route("/data_sync")
+def get_data_sync():
+    for i in range(10):
+        get_data_func_internal(i)
+    data = ''
+    return render_template('async_check.html', data=data)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html', data_sync=data_list_sync, data_async=data_list_async)
 
 
 if __name__ == '__main__':
